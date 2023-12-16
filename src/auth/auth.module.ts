8ac1724fb6b30d3ea3cc22c1c5c50008c4f0738b
Tom <tomasvergara272@gmail.com>
2023-12-16 20:37:10 -0300

    Entities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserSchema, User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
